a592839c7eb1775bd53b9c064bee9b30ef8ab8bb
nandareza <61969054+nandareza@users.noreply.github.com>
2024-02-07 00:55:23 +0800

    Update c5.R

--- rcode_C5/c5.R
+++ rcode_C5/c5.R
@@ -1,9 +1,9 @@
 install.packages("rlang")
 library(C50)
 
-m1 <- C5.0(output_oversampled[1:3257,-7], output_oversampled[1:3257,7])
+m1 <- C5.0(output[1:3257,-7], output[1:3257,7])
 m1
 summary (m1)
-p1 <-predict (m1, output_oversampled[3258:4653,])
+p1 <-predict (m1, output[3258:4653,])
 p1
 table(output_oversampled[3258:4653,7], predicted = p1)
